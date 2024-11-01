1c91a82d534bd804bfa6fddfb0f5dbb624aa347e
ph-3 <aaprashant2005@gmail.com>
2024-11-01 13:53:50 +0530

    First React Project

---
+++ my-first-react/src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+export default function App() {
+  return (
+    <>
+    <Header/>
+    <div className=" text-lg text-red-900  sm:text-yellow-600  md:text-green-500 lg:text-pink-300">
+      Hiiu
+    </div>
+<Footer/>
+    </>
+
+  )
+}
---
+++ my-first-react/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="flex justify-center border border-black">
+
+        <div className='p-4 '>Home</div>
+        <div className='p-4'>Account</div>
+        <div className='p-4'>Login</div>
+
+    </div>
+  )
+}
+
+export default Header
